e1618ea864b692cbfaca65d53e57b570edc3715d
Muhammad Reza <muhammad.reza@indocyber.co.id>
2022-03-28 18:01:44 +0700

    jwt author

--- Program.cs
+++ Program.cs
@@ -1,10 +1,57 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MiniApiJWT.InterfaceServices;
 using MiniApiJWT.Models;
 using MiniApiJWT.Services;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Description = "Bearer Authentication with JWT Token",
+        Type = SecuritySchemeType.Http
+    });
+    options.AddSecurityRequirement( new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateActor = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+builder.Services.AddAuthorization();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSingleton<IMovieService, MovieService>();
@@ -13,18 +60,74 @@ builder.Services.AddSingleton<IUserService, UserService>();
 var app = builder.Build();
 
 app.UseSwagger();
+app.UseAuthorization();
+app.UseAuthentication();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!")
+    .ExcludeFromDescription();
 
-app.MapPost("/create", (Movie movie, IMovieService service) => Create(movie, service));
+app.MapPost("/login", (UserLogin user, IUserService service) => Login(user, service))
+    .Accepts<UserLogin>("application/json")
+    .Produces<string>();
 
-app.MapGet("/get", (int id, IMovieService service) => Get(id, service));
+app.MapPost("/create", 
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+    (Movie movie, IMovieService service) => Create(movie, service))
+    .Accepts<UserLogin>("application/json")
+    .Produces<Movie>(statusCode: 200, contentType: "application/json");
 
-app.MapGet("/list", (IMovieService service) => List(service));
+app.MapGet("/get",
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Administrator")]
+    (int id, IMovieService service) => Get(id, service))
+    .Produces<Movie>();
 
-app.MapPut("/update", (Movie newMovie, IMovieService service) => Update(newMovie, service));
+app.MapGet("/list", (IMovieService service) => List(service))
+    .Produces<Movie>(statusCode: 200, contentType: "application/json"); ;
 
-app.MapDelete("/delete", (int id, IMovieService service) => Delete(id, service));
+app.MapPut("/update",
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+    (Movie newMovie, IMovieService service) => Update(newMovie, service))
+    .Accepts<Movie>("application/json");
+
+app.MapDelete("/delete",
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+    (int id, IMovieService service) => Delete(id, service));
+
+IResult Login(UserLogin user, IUserService service)
+{
+    if (!string.IsNullOrEmpty(user.UserName)&&
+        !string.IsNullOrEmpty(user.Password))
+    {
+        var loggedInUser = service.Get(user);
+        if (loggedInUser is null) return Results.NotFound("User not found");
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, loggedInUser.UserName),
+            new Claim(ClaimTypes.Email, loggedInUser.EmailAddress),
+            new Claim(ClaimTypes.GivenName, loggedInUser.GivenName),
+            new Claim(ClaimTypes.Surname, loggedInUser.SurName),
+            new Claim(ClaimTypes.Role, loggedInUser.Role)
+        };
+
+        var token = new JwtSecurityToken
+        (
+            issuer: builder.Configuration["Jwt:Issuer"],
+            audience: builder.Configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddDays(60),
+            notBefore: DateTime.UtcNow,
+            signingCredentials: new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                SecurityAlgorithms.HmacSha256)
+        );
+
+        var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return Results.Ok(tokenString);
+    }
+    return Results.NotFound("User not found");
+}
 
 IResult Create(Movie movie, IMovieService service)
 {
